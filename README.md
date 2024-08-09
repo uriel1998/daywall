9653100f6e4c5c5b0ec77e5757b9ad315d8392a3
Steven Saus <steven@stevesaus.com>
2024-08-09 10:35:47 -0400

    Starting with image brightness checker for more dynamic usage

---
+++ README.md
@@ -0,0 +1,38 @@
+# mpdq
+
+
+
+## Contents
+ 1. [About](#1-about)
+ 2. [License](#2-license)
+ 3. [Prerequisites](#3-prerequisites)
+ 4. [Installation](#4-installation)
+ 5. [Setup](#5-setup)
+ 6. [Usage](#6-usage)
+ 7. [TODO](#7-todo)
+
+***
+
+## 1. About
+
+
+## 2. License
+
+This project is licensed under the MIT License. For the full license, see `LICENSE`.
+
+## 3. Prerequisites
+
+
+## 4. Installation
+
+
+
+## 5. Setup
+
+
+## 6. Usage
+
+
+
+## 7. TODO
+ 
---
+++ image_brightness_checker.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# initializing arrays
+export ImageFile=()
+export ImageBright=()
+
+if [ -d "${1}" ];then
+    DIRECTORY="${1}"
+else
+    DIRECTORY="${PWD}"
+fi
+
+cd "${DIRECTORY}"
+imgfiles=$(fdfind --exclude '*tile*' -a -0 -e jpg -e jpeg -e png | xargs --null -I {} realpath {} )
+
+rm ~/imagebright.csv
+
+while read -r line; do
+    # echo "${line}"
+    if [ -f "${line}" ];then 
+        filename=$(basename "${line}")
+        OIFS=$IFS
+        IFS=$'\n'; set -f
+        brightcolor=$(timeout 5 convert "${line}" -colorspace Gray -format "%[fx:quantumrange*image.mean]" info:)
+        # rounding the number, crudely.
+        NUMBER=$(echo $brightcolor | awk '{ print $0 + .90 }')
+        NUMBER=$(printf "%0.f" $NUMBER)
+        ImageFile+=("${line}")
+        ImageBright+=("${NUMBER}")
+        printf "%s\t%s\t%s\n" "${filename}" "${brightcolor}" "${NUMBER}"
+        printf "%s,%s,%s\n" "${line}" "${brightcolor}" "${NUMBER}" >> ~/imagebright.csv
+        # adding for thumbnails for example 
+        # this will eventually be the first thumbnailing and range run
+        timeout 5 convert -resize 50x50! "${line}" /home/steven/test/"${NUMBER}".jpg
+        IFS=OIFS
+    fi
+done < <(echo "${imgfiles}")
+# initial run to find full range (omitting missed and obviously false values)
+# (600 - 60024)
+# so here's what we do 
+# - find whether the sun is up
+# - find if it's overcast (maybe?)
+# - choose appropriate brightness level range
+# - find random file, test it's brightness. If within range, great! otherwise, repeat
