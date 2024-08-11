1f82d43ecaa67db05ffd3943a0610af8d61c3019
Steven Saus <steven@stevesaus.com>
2024-08-11 19:14:57 -0400

    brightness selection in place

--- daywall.sh
+++ daywall.sh
@@ -2,7 +2,7 @@
 
  ########################################################################
  # 
- #   MPDQ - The MPD Queuer
+ #   Daywall - A time/brightness based background changer
  #   by Steven Saus (c)2024
  #   Licensed under the MIT license
  #
@@ -12,10 +12,10 @@
 # Definitions
 ########################################################################
 
-ConfigDir=${XDG_CONFIG_HOME:-$HOME/.config}/daywall
+ConfigDir=${XDG_CONFIG_HOME:-$HOME/.config}
 ConfigFile=${ConfigDir}/daywall.ini
-LowValue=""
-HighValue=""
+CacheDir=${XDG_CACHE_HOME:-$HOME/.local/state}
+CacheFile=${ConfigDir}/daywall.cache
 ImageDir=""
 export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
@@ -29,17 +29,28 @@ function loud() {
     fi
 }
 
+# help
+# $1 - if directory, add it to cache and selectable images
+# 
+
 # function to read INI
-# should just have the image dir to scan
-# and then have a LOW and HIGH value
+    # should just have the image dir to scan
+    # maaaaaaaaaaaaaaaaaaaybe have a way to set it?
 
 # function to scan image dir
-    # find LOW and HIGH
+    # scan the directory in the ini file; if filename is not in our cache, analyze it and add
+    # scan a directory passed in $1; if filename is not in our cache, analyze it AND ADD
     # write list of images to scan from later
+    
+    
+    
 
 function time_of_day() {
+    # get geolocated coordinates
+    # TODO: Ensure there aren't coords set by environment variable
     
     coords=$(curl -s https://whatismycountry.com/ | sed -e 's/picture/\n/g' -e 's/&#176;//g'  | grep "My coordinates" | awk -F '>' '{print $5}' | awk -F '<' ' {print $1}')
+    
     # TODO: test to make sure that's not junk and we're connected to the internet
     lat=$(echo "${coords}" | awk -F ', ' '{ print $1 }')
     long=$(echo "${coords}" | awk -F ', ' '{ print $2 }')    
@@ -53,66 +64,62 @@ function time_of_day() {
         midnight=$(echo "$midnight-24" | bc)
     fi
     
-    # where is current hour in comparison to midnight and midday
+    # where is current hour in comparison to midday
     currhour=$(date "+%-H")
-
+    time_diff=$(expr $(date +%Y%m%d)${currhour} - $(date +%Y%m%d)${midday})
+    abs_time_diff=${time_diff#-}
     
-    # 0 - 60,000 (or so) is about 5,000 an hour
-    # which perceptually is a bit much, so maybe 4k for midnight to + 6
-    # then 6k for the 6 hrs to noon, then back down
-    if [ $currhour -le
-0: 4000
-1: 8000
-2: 12000
-3: 16000
-4: 20000
-5: 24000
-6: 30000
-7: 36000
-8: 42000
-9: 48000
-10: 54000
-11: 65000
+    # map the high and low value for the image for the appropriate time
+    case "${abs_time_diff}" in
+        0)  highval=65000    
+            lowval=54000
+            ;;
+        1)  highval=54000
+            lowval=48000
+            ;;
+        2)  highval=48000
+            lowval=42000
+            ;;
+        3)  highval=42000
+            lowval=36000
+            ;;
+        4)  highval=36000
+            lowval=30000
+            ;;
+        5)  highval=30000
+            lowval=24000
+            ;;
+        6)  highval=24000
+            lowval=20000
+            ;;
+        7)  highval=20000
+            lowval=16000
+            ;;
+        8)  highval=16000
+            lowval=12000
+            ;;
+        9)  highval=12000
+            lowval=8000
+            ;;
+        10) highval=8000
+            lowval=4000
+            ;;
+        11) highval=4000
+            lowval=2000
+            ;;
+        12) highval=2000
+            lowval=200
+            ;;
+        *)  highval=2000
+            lowval=200
+            ;;
+    esac
 
-}
+    # randomly choose line from file with brightness values
+    # if brightness value -le highval or -ge lowval, then output (or pass to feh
+    # or whatever)
 
 
-currhour=$(date "+%-H")
-echo $currhour
-tod=$(date "+%p")
-case "$tod" in
-    "AM")
-    risehour=$(sunrise `date "+%Y %m %d"` 39.45 84.11 | awk '{print $3}'|awk -F ":" '{print $1}')  # When is the hour of sunrise
-    starttwi=$(echo "scale=0; $risehour-1" | bc)  # hour before and after sunrise hour
-    endtwi=$(echo "scale=0; $risehour+1" | bc)
-    echo $starttwi $endtwi
-    if [ "$currhour" -gt "$endtwi" ]; then
-        flavor="day"
-    elif [ "$currhour" -lt "$starttwi" ]; then
-        flavor="night"
-    else
-        flavor="trans"
-    fi
-    echo $flavor
-    ;;
-    "PM")
-    sethour=$(sunrise `date "+%Y %m %d"` 39.45 84.11 | awk '{print $3}'|awk -F ":" '{print $1}')
-    starttwi=$(echo "scale=0; $sethour-1+12" | bc)
-    endtwi=$(echo "scale=0; $sethour+1+12" | bc)
-    echo $starttwi $endtwi
-    if [ "$currhour" -gt "$endtwi" ]; then
-        flavor="nigh"
-    elif [ "$currhour" -lt "$starttwi" ]; then
-        flavor="day"
-    else
-        flavor="trans"
-    fi
-    echo $flavor    
-    ;;  
-esac
-
-dir="/home/USERDIRECTORY/.backgrounds/blacknwhite" # Directory
-file=`ls -1 /home/USERDIRECTORY/.backgrounds/blacknwhite/$flavor* | sort --random-sort | head -1`
 export DISPLAY=:0.0;feh --bg-scale $file
 echo "The randomly-selected file is: $file"
 exit
