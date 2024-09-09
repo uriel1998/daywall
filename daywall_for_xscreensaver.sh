b4f63379c52899a07226f7dbc7d0cfd340031497
Steven Saus <steven@stevesaus.com>
2024-09-08 21:59:32 -0400

    Updated to avoid duplicates

--- daywall_for_xscreensaver.sh
+++ daywall_for_xscreensaver.sh
@@ -30,12 +30,26 @@ if [ ! -d "${SCREENSAVERDIR}" ];then
     mkdir -p "${SCREENSAVERDIR}"
 fi
 
-# find and delete ONLY SYMLINKS in the specified directory
+
+CURR_FILE_LIST=""
+
+
+
+
+
+
+# find and delete files in the specified directory
 find "${SCREENSAVERDIR}" -maxdepth 1 -iname "*daywall_screensaver_*" -type f -delete
 counter=0
 while [ $counter -lt 9 ]; do
     file=$("${SCRIPT_DIR}"/daywall.sh --no-update)
+    #  If it's already been chosen in this round, kick it back
+    if [[ $CURR_FILE_LIST == *"$file"* ]];then 
+        file=""
+    fi
+    
     if [ -f "${file}" ];then 
+        CURR_FILE_LIST+="$file"
         convert "${file}" "${SCREENSAVERDIR}"/daywall_screensaver_"${counter}".jpg
         ((counter++))
     fi
